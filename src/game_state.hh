f7be1c1dd5885566c0514ed4208e187a0d6a4222
Sacha Delanoue <contact@shaac.me>
2017-03-24 13:42:31 +0100

    GameState: add public methods

--- src/game_state.hh
+++ src/game_state.hh
@@ -87,6 +87,66 @@ public:
     rules::GameState* copy() const override;
     ~GameState();
 
+    void increment_turn() { turn_++; }
+    int get_turn() const { return turn_; }
+    unsigned opponent(unsigned player) const
+    {
+        return (apprentices_ids_[0] == player) ? apprentices_ids_[1]
+                                               : apprentices_ids_[0];
+    }
+
+    /// Reset all inner state variables only valid during a turn.
+    void reset_turn_state();
+
+    /// Get a player's score. The id must be valid.
+    int get_score(unsigned apprentice_id) const;
+
+    /// Get number of catalysts available at the moment.
+    int get_catalysts_number() const { return catalysts_; }
+
+    /// Get the sample for the turn.
+    echantillon current_sample() const { return sample_; }
+
+    /// Tell if the sample was already placed.
+    bool is_sample_placed() const { return sample_placed_; }
+
+    /// Place a sample on a workbench.
+    void place_sample(position pos1, position pos2, unsigned apprentice_id);
+
+    /// Activate the region at a given position.
+    void transmute(position pos, unsigned apprentice_id);
+
+    /// Change element on given case. Consume a catalyst.
+    void catalyze(position pos, unsigned apprentice_id, case_type new_type);
+
+    /// Set the sample for the opponent's next turn.
+    void give(echantillon sample);
+
+    /// Get the type of a case at a given position.
+    /// Return 'vide' if the position is invalid.
+    case_type get_cell_type(position pos, unsigned apprentice_id) const;
+
+    /// Get the size of the region containing the position.
+    /// Return -1 if position is invalid.
+    int get_region_size(position pos, unsigned apprentice_id) const;
+
+    /// Get the position of all cases of a region containing a given position
+    std::vector<position> get_region(position p, unsigned apprentice_id) const;
+
+    /// Get all the posible locations for a sample an a workbench
+    std::vector<position_echantillon>
+    possible_sample_positions(echantillon sample, unsigned apprentice_id) const;
+
+    // Update action history
+    void hist_add_place(position pos1, position pos2, unsigned apprentice_id);
+    void hist_add_transmute(position pos, unsigned apprentice_id);
+    void hist_add_catalize(position pos, unsigned target_apprentice_id,
+                           case_type new_type, unsigned apprentice_id);
+    void hist_add_give(echantillon sample, unsigned apprentice_id);
+    void reset_history(unsigned apprentice_id);
+
+    const std::vector<action>& get_history(unsigned apprentice_id) const;
+
 private:
     std::unordered_map<unsigned, Apprentice> apprentices_;
     std::array<unsigned, 2> apprentices_ids_;
@@ -103,6 +163,7 @@ private:
     int catalysts_;
     bool sample_placed_;
     echantillon sample_;
+    echantillon next_sample_;
 };
 
 #endif /* !GAME_STATE_HH */
