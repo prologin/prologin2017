5136ed123589aa865ab9cae73c696984af7e280d
Sacha Delanoue <contact@shaac.me>
2017-03-24 15:16:04 +0100

    GameState: implement methods (some just skeleton)

--- src/game_state.cc
+++ src/game_state.cc
@@ -57,3 +57,121 @@ void GameState::reset_turn_state()
     sample_placed_ = false;
     sample_ = next_sample_;
 }
+
+int GameState::get_score(unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    return apprentices_.at(apprentice_id).get_score();
+}
+
+void GameState::place_sample(position pos1, position pos2,
+                             unsigned apprentice_id)
+{
+    assert(!sample_placed_);
+    assert(apprentices_.count(apprentice_id) != 0);
+    // assert(workbenches_.at(id)[pos1.ligne][pos1.colonne] == VIDE);
+    // assert(workbenches_.at(id)[pos2.ligne][pos2.colonne] == VIDE);
+    // FIXME
+}
+
+void GameState::transmute(position pos, unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // FIXME
+}
+
+void GameState::catalyze(position pos, unsigned apprentice_id,
+                         case_type new_type)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // FIXME
+}
+
+void GameState::give(echantillon sample)
+{
+    assert(sample.element1 != VIDE);
+    assert(sample.element2 != VIDE);
+    next_sample_ = sample;
+}
+
+case_type GameState::get_cell_type(position pos, unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // return workbenches_[id][pos.ligne][pos.colonne];
+    // FIXME
+}
+
+int GameState::get_region_size(position pos, unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // FIXME
+}
+
+std::vector<position> GameState::get_region(position p,
+                                            unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // FIXME
+}
+
+std::vector<position_echantillon>
+GameState::possible_sample_positions(echantillon sample,
+                                     unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    // FIXME
+}
+
+void GameState::hist_add_place(position pos1, position pos2,
+                               unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    action a;
+    a.type = action_type::PLACE;
+    a.pos1 = pos1;
+    a.pos2 = pos2;
+    apprentices_.at(apprentice_id).add_action(a);
+}
+
+void GameState::hist_add_transmute(position pos, unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    action a;
+    a.type = action_type::TRANSMUTE;
+    a.pos1 = pos;
+    apprentices_.at(apprentice_id).add_action(a);
+}
+
+void GameState::hist_add_catalize(position pos, unsigned target_apprentice_id,
+                                  case_type new_type, unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    action a;
+    a.type = action_type::CATALYSE;
+    a.pos1 = pos;
+    a.apprentice_id = target_apprentice_id;
+    a.case1 = new_type;
+    apprentices_.at(apprentice_id).add_action(a);
+}
+
+void GameState::hist_add_give(echantillon sample, unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    action a;
+    a.type = action_type::GIVE;
+    a.case1 = sample.element1;
+    a.case2 = sample.element2;
+    apprentices_.at(apprentice_id).add_action(a);
+}
+
+void GameState::reset_history(unsigned apprentice_id)
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    apprentices_.at(apprentice_id).reset_actions();
+}
+
+const std::vector<action>& GameState::get_history(unsigned apprentice_id) const
+{
+    assert(apprentices_.count(apprentice_id) != 0);
+    return apprentices_.at(apprentice_id).get_actions();
+}
