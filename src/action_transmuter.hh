f687c03f125ec645c1457b33c5a5d3ab8bd9ed77
Sacha Delanoue <contact@shaac.me>
2017-03-24 11:56:33 +0100

    Add license

--- src/action_transmuter.hh
+++ src/action_transmuter.hh
@@ -1,3 +1,22 @@
+/*
+** This file is part of Prologin2017, a rules library for stechec2.
+**
+** Copyright (c) 2017 Association Prologin <info@prologin.org>
+**
+** Prologin2017 is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Prologin2017 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Prologin2017.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ACTION_TRANSMUTER_HH
 #define ACTION_TRANSMUTER_HH
 
--- src/interface.cc
+++ src/interface.cc
@@ -1,13 +1,20 @@
 /*
-** Stechec project is free software; you can redistribute it and/or modify
+** This file is part of Prologin2017, a rules library for stechec2.
+**
+** Copyright (c) 2017 Association Prologin <info@prologin.org>
+**
+** Prologin2017 is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
+** the Free Software Foundation, either version 3 of the License, or
 ** (at your option) any later version.
 **
-** The complete GNU General Public Licence Notice can be found as the
-** `NOTICE' file in the root directory.
+** Prologin2017 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
 **
-** Copyright (C) 2017 Prologin
+** You should have received a copy of the GNU General Public License
+** along with Prologin2017.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "api.hh"
