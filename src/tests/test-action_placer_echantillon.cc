2fc95ae9149bd3d6945c0f8cc4151e74586e7996
Cbeausei <cbeausei@163-172-59-150.online.net>
2017-03-25 11:17:29 +0100

    tests: add some tests for action placer_echantillon

--- src/tests/test-action_placer_echantillon.cc
+++ src/tests/test-action_placer_echantillon.cc
@@ -12,3 +12,49 @@ TEST_F(ActionTest, PlacerEchantillon_InvalidCell)
     EXPECT_EQ(POSITION_INVALIDE, act->check(gs_));
     delete act;
 }
+
+TEST_F(ActionTest, PlacerEchantillon_NotAdjacent)
+{
+    ActionPlacerEchantillon* act;
+
+    act = new ActionPlacerEchantillon({1, 1}, {2, 2}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_INVALIDE, act->check(gs_));
+    delete act;
+
+    act = new ActionPlacerEchantillon({1, 1}, {1, 1}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_INVALIDE, act->check(gs_));
+    delete act;
+}
+
+TEST_F(ActionTest, PlacerEchantillon_Impossible)
+{
+    ActionPlacerEchantillon* act;
+
+    act = new ActionPlacerEchantillon({1, 1}, {2, 1}, PLAYER_1);
+    EXPECT_EQ(OK, act->check(gs_));
+    act->apply_on(gs_);
+    delete act;
+
+    gs_->reset_turn_state();
+
+    act = new ActionPlacerEchantillon({2, 2}, {3, 2}, PLAYER_1);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionPlacerEchantillon({1, 1}, {2, 1}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_IMPOSSIBLE, act->check(gs_));
+    delete act;
+
+    act = new ActionPlacerEchantillon({1, 1}, {1, 2}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_IMPOSSIBLE, act->check(gs_));
+    delete act;
+
+    act = new ActionPlacerEchantillon({2, 2}, {2, 1}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_IMPOSSIBLE, act->check(gs_));
+    delete act;
+
+    act = new ActionPlacerEchantillon({2, 1}, {1, 1}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_IMPOSSIBLE, act->check(gs_));
+    delete act;
+
+}
