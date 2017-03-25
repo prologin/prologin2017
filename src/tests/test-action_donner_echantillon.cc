5bda5fe6d047d4384a6f9bbf8ad427e30acdc9a4
Cbeausei <cbeausei@163-172-59-150.online.net>
2017-03-25 10:48:19 +0100

    tests: donner_echantillon: add complete tests

--- src/tests/test-action_donner_echantillon.cc
+++ src/tests/test-action_donner_echantillon.cc
@@ -20,3 +20,57 @@ TEST_F(ActionTest, DonnerEchantillon_Invalid)
     EXPECT_EQ(ECHANTILLON_INCOMPLET, act->check(gs_));
     delete act;
 }
+
+TEST_F(ActionTest, DonnerEchantillon_MatchingPrevious)
+{
+    ActionDonnerEchantillon* act;
+
+    gs_->set_next_sample({MERCURE, SOUFRE});
+    gs_->reset_turn_state();
+
+    act = new ActionDonnerEchantillon({MERCURE, FER}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({SOUFRE, FER}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({CUIVRE, MERCURE}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({CUIVRE, SOUFRE}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({MERCURE, SOUFRE}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({SOUFRE, MERCURE}, PLAYER_2);
+    EXPECT_EQ(OK, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({CUIVRE, FER}, PLAYER_2);
+    EXPECT_EQ(ECHANTILLON_INVALIDE, act->check(gs_));
+    delete act;
+
+    act = new ActionDonnerEchantillon({PLOMB, PLOMB}, PLAYER_2);
+    EXPECT_EQ(ECHANTILLON_INVALIDE, act->check(gs_));
+    delete act;
+}
+
+TEST_F(ActionTest, DonnerEchantillon_UpdateGamestate)
+{
+    ActionDonnerEchantillon* act;
+
+    gs_->set_next_sample({MERCURE, SOUFRE});
+    gs_->reset_turn_state();
+
+    act = new ActionDonnerEchantillon({FER, MERCURE}, PLAYER_1);
+    EXPECT_EQ(OK, act->check(gs_));
+    act->apply_on(gs_);
+    EXPECT_EQ(true, gs_->was_sample_given());
+    delete act;
+}
