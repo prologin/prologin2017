11a45a55f638afa3a4e4d3325fa7761d59a140a9
Cbeausei <cbeausei@163-172-59-150.online.net>
2017-03-26 16:42:16 +0200

    tests: placer_echantillon: add tests for connexity

--- src/tests/test-action_placer_echantillon.cc
+++ src/tests/test-action_placer_echantillon.cc
@@ -46,3 +46,32 @@ TEST_F(ActionTest, PlacerEchantillon_Impossible)
     ActionPlacerEchantillon act6({2, 1}, {1, 1}, PLAYER_1);
     EXPECT_EQ(PLACEMENT_IMPOSSIBLE, act6.check(gs_));
 }
+
+TEST_F(ActionTest, PlacerEchantillon_Connexity)
+{
+    gs_->set_next_sample({MERCURE, FER});
+    gs_->reset_turn_state();
+
+    ActionPlacerEchantillon act({1, 1}, {2, 1}, PLAYER_1);
+    EXPECT_EQ(OK, act.check(gs_));
+    act.apply_on(gs_);
+
+    gs_->set_next_sample({MERCURE, PLOMB});
+    gs_->reset_turn_state();
+
+    ActionPlacerEchantillon act2({2, 2}, {3, 2}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_INCORRECT, act2.check(gs_));
+
+    ActionPlacerEchantillon act3({1, 2}, {2, 2}, PLAYER_1);
+    EXPECT_EQ(OK, act3.check(gs_));
+
+    ActionPlacerEchantillon act4({0, 1}, {0, 0}, PLAYER_1);
+    EXPECT_EQ(OK, act4.check(gs_));
+
+    ActionPlacerEchantillon act5({0, 0}, {0, 1}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_INCORRECT, act5.check(gs_));
+
+    ActionPlacerEchantillon act6({2, 2}, {1, 2}, PLAYER_1);
+    EXPECT_EQ(PLACEMENT_INCORRECT, act6.check(gs_));
+}
+
