7d3c7effe9b276e4f4f9259cbef2017b82350447
Cbeausei <cbeausei@cbeausei.online.net>
2017-03-30 01:13:29 +0200

    tests: add tests for the api

---
+++ src/tests/test-api.cc
@@ -0,0 +1,94 @@
+/*
+** This file is part of Prologin2016, a rules library for stechec2.
+**
+** Copyright (c) 2016 Association Prologin <info@prologin.org>
+**
+** Prologin2016 is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Prologin2016 is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Prologin2016.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "../api.hh"
+
+#include "test-helpers.hh"
+
+TEST_F(ApiTest, Api_TypeCase)
+{
+    // EXPECT_EQ(VIDE, players[0].api->type_case({-1, -1}, players[0].id));
+    EXPECT_EQ(VIDE, players[0].api->type_case({1, 1}, players[0].id));
+    gs_->set_next_sample({MERCURE, PLOMB});
+    gs_->reset_turn_state();
+    gs_->place_sample({1, 1}, {1, 2}, players[0].id);
+    EXPECT_EQ(MERCURE, players[0].api->type_case({1, 1}, players[0].id));
+    EXPECT_EQ(PLOMB, players[0].api->type_case({1, 2}, players[0].id));
+    EXPECT_EQ(VIDE, players[0].api->type_case({1, 2}, players[1].id));
+}
+
+TEST_F(ApiTest, Api_EstVide)
+{
+    // EXPECT_TRUE(players[0].api->est_vide({-1, -1}, players[0].id));
+    EXPECT_TRUE(players[0].api->est_vide({1, 1}, players[0].id));
+    gs_->set_next_sample({MERCURE, PLOMB});
+    gs_->reset_turn_state();
+    gs_->place_sample({1, 1}, {1, 2}, players[0].id);
+    EXPECT_FALSE(players[0].api->est_vide({1, 1}, players[0].id));
+    EXPECT_FALSE(players[0].api->est_vide({1, 2}, players[0].id));
+    EXPECT_TRUE(players[0].api->est_vide({1, 2}, players[1].id));
+}
+
+TEST_F(ApiTest, Api_ProprieteCase)
+{
+    // EXPECT_TRUE(players[0].api->propriete_case({-1, -1}, players[0].id));
+    EXPECT_EQ(AUCUNE, players[0].api->propriete_case({1, 1}, players[0].id));
+    gs_->set_next_sample({MERCURE, PLOMB});
+    gs_->reset_turn_state();
+    gs_->place_sample({1, 1}, {1, 2}, players[0].id);
+    EXPECT_EQ(TRANSMUTABLE_CATALYSEUR,
+              players[0].api->propriete_case({1, 1}, players[0].id));
+    EXPECT_EQ(TRANSMUTABLE_OR,
+              players[0].api->propriete_case({1, 2}, players[0].id));
+    EXPECT_EQ(AUCUNE, players[0].api->propriete_case({1, 2}, players[1].id));
+}
+
+TEST_F(ApiTest, Api_ProprieteCaseType)
+{
+    EXPECT_EQ(AUCUNE, players[0].api->propriete_case_type(VIDE));
+    EXPECT_EQ(TRANSMUTABLE_OR, players[0].api->propriete_case_type(PLOMB));
+    EXPECT_EQ(TRANSMUTABLE_OR, players[0].api->propriete_case_type(FER));
+    EXPECT_EQ(TRANSMUTABLE_OR, players[0].api->propriete_case_type(CUIVRE));
+    EXPECT_EQ(TRANSMUTABLE_CATALYSEUR,
+              players[0].api->propriete_case_type(SOUFRE));
+    EXPECT_EQ(TRANSMUTABLE_CATALYSEUR,
+              players[0].api->propriete_case_type(MERCURE));
+}
+
+TEST_F(ApiTest, Api_TailleRegion)
+{
+    EXPECT_EQ(0, players[0].api->taille_region({1, 1}, players[0].id));
+
+    gs_->set_next_sample({MERCURE, MERCURE});
+    gs_->reset_turn_state();
+    gs_->place_sample({1, 1}, {1, 2}, players[0].id);
+    EXPECT_EQ(2, players[0].api->taille_region({1, 1}, players[0].id));
+    EXPECT_EQ(2, players[0].api->taille_region({1, 2}, players[0].id));
+
+    gs_->set_next_sample({CUIVRE, MERCURE});
+    gs_->reset_turn_state();
+    gs_->place_sample({2, 1}, {2, 2}, players[0].id);
+    EXPECT_EQ(1, players[0].api->taille_region({2, 1}, players[0].id));
+    EXPECT_EQ(3, players[0].api->taille_region({1, 1}, players[0].id));
+
+    gs_->set_next_sample({MERCURE, MERCURE});
+    gs_->reset_turn_state();
+    gs_->place_sample({3, 2}, {4, 2}, players[0].id);
+    EXPECT_EQ(5, players[0].api->taille_region({4, 2}, players[0].id));
+}
--- src/tests/test-helpers.hh
+++ src/tests/test-helpers.hh
@@ -23,17 +23,50 @@ static rules::Players_sptr make_players(int id1, int id2)
 
 class ActionTest : public ::testing::Test
 {
-    protected:
-        virtual void SetUp()
-        {
-            utils::Logger::get().level() = utils::Logger::DEBUG_LEVEL;
-            gs_ = new GameState(make_players(PLAYER_1, PLAYER_2));
-        }
+protected:
+    virtual void SetUp()
+    {
+        utils::Logger::get().level() = utils::Logger::DEBUG_LEVEL;
+        gs_ = new GameState(make_players(PLAYER_1, PLAYER_2));
+    }
 
-        GameState* gs_;
+    GameState* gs_;
 
-        const int PLAYER_1 = 42;
-        const int PLAYER_2 = 1337;
+    const int PLAYER_1 = 42;
+    const int PLAYER_2 = 1337;
+};
+
+class ApiTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        // Players values are not 0 and 1, because that would be too simple
+        int player_id_1 = 1337;
+        int player_id_2 = 42;
+        utils::Logger::get().level() = utils::Logger::DEBUG_LEVEL;
+        auto players_ptr = make_players(player_id_1, player_id_2);
+        gs_ = new GameState(make_players(player_id_1, player_id_2));
+        players[0].id = player_id_1;
+        players[0].api = new Api(gs_, players_ptr->players[0]);
+        players[1].id = player_id_2;
+        players[1].api = new Api(gs_, players_ptr->players[1]);
+    }
+
+    virtual void TearDown()
+    {
+        delete players[0].api;
+        delete players[1].api;
+    }
+
+    GameState* gs_;
+
+    struct Player
+    {
+        int id;
+        Api* api;
+    };
+    std::array<Player, 2> players;
 };
 
 #endif
